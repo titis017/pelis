c1b43628a66122f0a95f822d126f482ea418ddc3
Pedro <ttittis2004@gmail.com>
2023-11-12 04:03:11 +0100

    Js recoge los datos correctamente y se muestran correctamente en la pagina sin ningun estilo correcto

--- functions.js
+++ functions.js
@@ -6,14 +6,24 @@ createApp({
     data()  {
         return{
             datos: [],
+            detPeli: [],
         }
     },
     methods: {
-        getInfoPeli(id){
-            fetch("http://www.omdbapi.com/?i=${id}&plot=full&apikey=ef59fad")
-            .then(response => response.json())
-            .then(console.log(response));
-        }
+        getInfoPeli(id) {
+            const apiUrl = `http://www.omdbapi.com/?i=${id}&plot=full&apikey=ef59fad`;
+          
+            return fetch(apiUrl)
+              .then(response => response.json())
+              .then(data => {
+                console.log(data);
+                this.detPeli = data;
+              })
+              .catch(error => {
+                console.error('Error:', error);
+                throw error; 
+              });
+          }
     },
     created(){
         getPelis().then(data =>  {
