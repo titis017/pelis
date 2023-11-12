cd35704414151c4ba063c9384f320cae5e594f12
Pedro <ttittis2004@gmail.com>
2023-11-12 03:32:08 +0100

    Modal funcional sin cuerpo

--- functions.js
+++ functions.js
@@ -9,7 +9,11 @@ createApp({
         }
     },
     methods: {
-
+        getInfoPeli(id){
+            fetch("http://www.omdbapi.com/?i=${id}&plot=full&apikey=ef59fad")
+            .then(response => response.json())
+            .then(console.log(response));
+        }
     },
     created(){
         getPelis().then(data =>  {
